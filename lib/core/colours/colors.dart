a436ceaa5179e65978dd19ee16ac031f4c88a0bb
akhilSNediyathu <akhilsnediyathu@gmail.com>
2024-04-04 15:33:24 +0530

    downloads completed

--- lib/core/colours/colors.dart
+++ lib/core/colours/colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const backgroundColor = Colors.black;
+const kwhiteColor = Colors.white;
+final kbuttonColorblue = Colors.blueAccent[700];
+const kbuttonColorwhite = Colors.white;
+const kblackColor = Colors.black;
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:netflix/core/colours/colors.dart';
 import 'package:netflix/presentaion/main_page/screen_mainpage.dart';
 
 void main() {
@@ -14,25 +16,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        scaffoldBackgroundColor:backgroundColor,
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+        bodyLarge: TextStyle(
+          color: Colors.white
+        ),bodyMedium: TextStyle(
+          color: Colors.white
+        ) 
+        ),
+        
+
       ),
       home: ScreenMainPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
